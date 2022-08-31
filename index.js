83830d8ea402368302915c4eceaf4a645dbbcda3
asiamohammed <mohamedasia021@gmail.com>
2022-08-31 07:44:36 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const isWin = record.find((myrecord) => myrecord.result === "W");
+    if (isWin) {
+        return isWin.year;
+    } else {
+        return undefined;
+    }
+}
